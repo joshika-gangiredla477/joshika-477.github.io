c8893c28404646f69492570a0d08a48446477e50
joshika-gangiredla477 <joshika.gangiredla@wsu.edu>
2026-03-12 06:42:15 +0000

    changed the CSS to improve the visual style of the pop up that comes once the game is over

--- tic-tac-toe/scripts.js
+++ tic-tac-toe/scripts.js
@@ -1,8 +1,20 @@
 // declare the board data for a game, using 3 arrays
 // "-" indicates unmarked, "x" indicates an X mark, "o" indicates an O mark
-let rowA = ["x", "x", "o"];
-let rowB = ["x", "o", "o"];
-let rowC = ["-", "o", "x"];
+let rowA = ["-", "-", "-"];
+let rowB = ["-", "-", "-"];
+let rowC = ["-", "-", "-"];
+
+//track who's turn it is
+let currentTurn = "x";
+
+//track number of turns left
+let remainingTurns = 9;
+
+//track if game is over
+let gameOver = false;
+
+//set up blank variable for current player DOM
+let currentPlayer;
 
 function checkGameboard(checkA, checkB, checkC) {
   console.log("Function Started"); //function started
@@ -35,7 +47,7 @@ function checkGameboard(checkA, checkB, checkC) {
   }
 
 
-  
+
 
   //check diagnoals
   if (
@@ -60,22 +72,88 @@ function checkGameboard(checkA, checkB, checkC) {
   return "d";
 }
 
+//function to handle clicks
+function clickSquare() {
 
 
 
-// get a handle on the DOM element to be updated with the outcome
-let gameOutputMsg = document.querySelector("#gameResult span");
+    //only if space is empty
+    if ( (this.innerHTML == "") && (!gameOver) ) {
+        
+        //set the space
+        this.innerHTML = currentTurn;
 
-// call your function checkGameboard() with the 3 rows
-let winState = checkGameboard(rowA, rowB, rowC);
+        //substract 1 frpm remaining turns
+        remainingTurns--;
+        console.log("Remaining turns:" + remainingTurns)
 
-// test the returned value of the function
-if (winState == "x") {
-  gameOutputMsg.innerHTML = "X wins";
-} else if (winState == "o") {
-  gameOutputMsg.innerHTML = "O wins";
-} else if (winState == "d") {
-  gameOutputMsg.innerHTML = "draw";
-} else {
-  gameOutputMsg.innerHTML = "unknown";
+        //update the array of row with the player value
+        if (this.id == "a1") rowA[0] = currentTurn;
+        if (this.id == "a2") rowA[1] = currentTurn;
+        if (this.id == "a3") rowA[2] = currentTurn;
+
+        if (this.id == "b1") rowB[0] = currentTurn;
+        if (this.id == "b2") rowB[1] = currentTurn;
+        if (this.id == "b3") rowB[2] = currentTurn;
+
+        if (this.id == "c1") rowC[0] = currentTurn;
+        if (this.id == "c2") rowC[1] = currentTurn;
+        if (this.id == "c3") rowC[2] = currentTurn;
+
+        //OUTPUT arrays to console
+        console.log ("Rows:");
+        console.log (rowA);
+        console.log (rowB);
+        console.log (rowC);
+
+        // get a handle on the DOM element to be updated with the outcome
+        let gameOutputMsg = document.querySelector("#gameResult");
+
+        // call your function checkGameboard() with the 3 rows
+        let winState = checkGameboard(rowA, rowB, rowC);
+
+        // test the returned value of the function
+        if (winState == "x") {
+            gameOutputMsg.innerHTML = "X wins";
+            gameOver = true;
+        } else if (winState == "o") {
+            gameOutputMsg.innerHTML = "O wins";
+            gameOver = true;
+        } else if ((winState == "d") && (remainingTurns == 0)) {
+            gameOutputMsg.innerHTML = "draw";
+            gameOver = true;
+        } 
+
+        //reveal game outcome after game
+        if (gameOver) {
+            document.querySelector("#gameResult").style.display = "flex";
+        }
+
+        //flip turn back and forth
+        if (currentTurn == "x") currentTurn = "o";
+        else currentTurn = "x";
+
+
+
+        //update next player DOM element
+        currentPlayer.innerHTML = currentTurn;
+    }
+
+
 }
+
+//wait for the document to load before adding clickable events
+document.addEventListener("DOMContentLoaded", function(){
+
+    //find all the clickable spaces
+    let allSpaces = document.querySelectorAll(".gameSpace");
+
+    //loop with "for-of" through all the clickable spaces
+    for(let eachSpace of allSpaces) {
+        eachSpace.addEventListener("click", clickSquare);
+    }
+
+    //update current player DOM element with first player
+    currentPlayer = document.querySelector("#currentPlayer span");
+    currentPlayer.innerHTML = currentTurn;
+});
